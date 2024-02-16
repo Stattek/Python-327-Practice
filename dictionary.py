6833514c663dd45df105dc0aea4d4f7509f6ed62
Slaytekk <alienhuntsgames@gmail.com>
2024-02-15 21:43:34 -0600

    added more dictionary examples

--- dictionary.py
+++ dictionary.py
@@ -2,6 +2,22 @@ dictionary = {"key": "value", "hi": "hello", "test": "aaa"}
 
 print(dictionary)
 
+if "key" in dictionary:
+    print("true")
+
 for key in dictionary:
     print(key)
     print(dictionary[key])
+
+dictionary["ice"] = "cold"
+dictionary["fire"] = "hot"
+
+for key in dictionary:
+    print(key)
+    print(dictionary[key])
+
+# get all keys
+print(dictionary.keys())
+
+# get all values
+print(dictionary.values())
